e91a05e79f4767c7cc16840198f89f400b9d20fd
rhanciles <119437254+rhanciles@users.noreply.github.com>
2023-03-17 16:19:43 +0000

    2nd API call
    Add second API call with Longitude and Latitude input.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,40 @@
 var apiKey = "900fdcffb7a2a35ad536a57ccbc492e5";
-var cityName = $("#search-input").val()
-console.log(cityName)
-// // Here we are building the URL we need to query the database
-// // var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q" + cityName + "&limit=5&appid=" + apiKey;
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?&q=" + cityName + "&appid=" + apiKey;
+
+// var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q" + cityName + "&limit=5&appid=" + apiKey;
 
 $("#search-button").on("click", function(event) {
     event.preventDefault();
-    
+    var cityName = document.querySelector("#search-input").value.trim();
+    // var citySearch = document.querySelector('.form-input').value;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?&q=" + cityName + "&appid=" + apiKey;
+
     $.ajax({
-      url: "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=900fdcffb7a2a35ad536a57ccbc492e5",
+      url: queryURL,
       method: "GET"
     }).then(function(response) {
   
       console.log(response);
 
+      var latitude = response.coord.lat;
+      var longitude = response.coord.lon;
+  
+      console.log(latitude);
+      console.log(longitude);
+
+      var searchURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey + "&units=metric";
+
+      $.ajax({
+        url: searchURL,
+        method: "GET"
+      }).then(function(data) {
+    
+        console.log(data);
+   
+      });
+
     });
+
+    console.log(cityName);
+    console.log(apiKey);
 
 });
